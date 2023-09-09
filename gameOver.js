c3dfac0ea8fb9546f10ea8eac8763049c831a3fe
Aparnaa-k <aparnaa.kumar@kalvium.community>
2023-09-09 16:06:28 +0530

    added the js functionalities

---
+++ gameOver.js
@@ -0,0 +1,8 @@
+const scoreText = document.getElementById('scoreVal');
+let points = localStorage.getItem("score");
+scoreText.innerText=points;
+
+function redirectToGamePage(){
+    window.location = 'game_page.html'
+}
+
